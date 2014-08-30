5914d095c9c6e283da9c08d00527255329cdb7df
hideveloper <hi@regiongames.com>
2014-08-29 19:48:23 -0600

    Este es mi primer proyecto

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php echo "Hola Mundo esto es php"; ?>
+
+<html>
+    <head>
+        <title>Deploy en Heroku</title>
+    </head>
+    <body>
+        <p>Mi primer proyecto en Heroku + Github</p>
+        <?php echo "La suma de 5 + 5 = ". (5 + 5); ?>
+    </body>
+</html>
